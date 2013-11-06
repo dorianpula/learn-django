6e2390e164cac1cd798f83733815eab8b9bd457d
Dorian Pula <dorian.pula@amber-penguin-software.ca>
2013-11-05 22:55:24 -0500

    [Chapter 04]: Moved to using class based generic views. (p72)

--- cms/cms/urls.py
+++ cms/cms/urls.py
@@ -1,9 +1,18 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import YearArchiveView, MonthArchiveView, DayArchiveView, DateDetailView, ArchiveIndexView
+from charleston.models import Entry
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
+
 from cms.settings import PROJECT_HOME
+
+entry_info_dict = {
+    'queryset': Entry.objects.all(),
+    'date_field': 'publication_date',
+}
+
 urlpatterns = patterns(
     '',
     # Examples:
@@ -17,13 +26,16 @@ urlpatterns = patterns(
     url(r'^admin/', include(admin.site.urls)),
 
     # TinyMCE URLs...
-    (r'^tinymce/(?P<path>.*)$', 'django.views.static.serve', {'document_root':
+    url(r'^tinymce/(?P<path>.*)$', 'django.views.static.serve', {'document_root':
                                                               '{}/templates/js/tinymce'.format(PROJECT_HOME)}),
     url(r'^search/$', 'cms.search.views.search'),
 
-    url(r'^weblog/$', 'charleston.views.entries_index'),
+    url(r'^weblog/$', ArchiveIndexView.as_view(**entry_info_dict)),
+    url(r'^weblog/(?P<year>\d{4})/$', YearArchiveView.as_view(**entry_info_dict)),
+    url(r'^weblog/(?P<year>\d{4})/(?P<month>\w{3})/$', MonthArchiveView.as_view(**entry_info_dict)),
+    url(r'^weblog/(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/$', DayArchiveView.as_view(**entry_info_dict)),
     url(r'^weblog/(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$',
-        'charleston.views.entries_detail'),
+        DateDetailView.as_view(**entry_info_dict)),
 
     # Flatpages url.
     url(r'', include('django.contrib.flatpages.urls'))
