536703f9c1ad224c4a107e3a921ea3ee7afbeedf
Dorian Pula <dorian.pula@amber-penguin-software.ca>
2013-11-09 14:18:03 -0500

    [Chapter 05]: Added in the views for tags, links and categories. (p90)

--- charleston/urls.py
+++ charleston/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import patterns, url
 from django.views.generic import YearArchiveView, MonthArchiveView, DayArchiveView, ArchiveIndexView, DateDetailView
+from django.views.generic import ListView, DetailView
 
-from charleston.models import Entry
+from charleston.models import Category, Entry, Link
+from tagging.models import Tag
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -10,6 +12,7 @@ admin.autodiscover()
 urlpatterns = patterns(
     '',
 
+    # URLS for the blog posts.
     url(r'^$',
         ArchiveIndexView.as_view(queryset=Entry.objects.all(), date_field='pub_date')),
     url(r'^(?P<year>\d{4})/$',
@@ -20,5 +23,31 @@ urlpatterns = patterns(
         DayArchiveView.as_view(queryset=Entry.objects.all(), date_field='pub_date')),
     url(r'^(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$',
         DateDetailView.as_view(queryset=Entry.objects.all(), date_field='pub_date'),
-        name="charleston_entry_detail")
+        name="charleston_entry_detail"),
+
+    # URLs for the links.
+    url(r'links/^$',
+        ArchiveIndexView.as_view(queryset=Link.objects.all(), date_field='pub_date')),
+    url(r'^links/(?P<year>\d{4})/$',
+        YearArchiveView.as_view(queryset=Link.objects.all(), date_field='pub_date')),
+    url(r'^links/(?P<year>\d{4})/(?P<month>\w{3})/$',
+        MonthArchiveView.as_view(queryset=Link.objects.all(), date_field='pub_date')),
+    url(r'^links/(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/$',
+        DayArchiveView.as_view(queryset=Link.objects.all(), date_field='pub_date')),
+    url(r'^links/(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$',
+        DateDetailView.as_view(queryset=Link.objects.all(), date_field='pub_date'),
+        name="charleston_link_detail")
 )
+
+urlpatterns += patterns('charleston.views',
+                        (r'^categories/$', ListView.as_view(queryset=Category.objects.all())),
+                        (r'^categories/(?P<slug>[-\w]+)/$', 'category_detail'),)
+
+urlpatterns += patterns('',
+                        (r'^tags/$', ListView.as_view(queryset=Tag.objects.all())),
+                        (r'^tags/entries/(?P<tag>[-\w]+)/$', 'tagging.views.tagged_object_list',
+                         {'queryset_or_model': Entry, 'template_name': 'charleston/entries_by_tag.html'}),
+                        (r'^tags/links/(?P<tag>[-\w]+)/$', 'tagging.views.tagged_object_list',
+                         {'queryset_or_model': Link, 'template_name': 'charleston/links_by_tag.html'}),
+)
+
--- charleston/views.py
+++ charleston/views.py
@@ -1,1 +1,30 @@
-# And we went all class-based generic views on y'all.
+"""
+Views for the charleston project.
+
+"""
+from django.shortcuts import render_to_response, get_object_or_404
+
+from charleston.models import Category
+
+
+def category_list(request):
+    """
+    Render a list of categories.
+
+    :param request: The request to render.
+    """
+    return render_to_response('charleston/category_list.html', {'object_list': Category.objects.all()})
+
+
+def category_detail(request, slug):
+    """
+    Category details based on request or slug.
+
+    :param request: The request to serve.
+    :param slug: The slug for the category.
+    """
+
+    category = get_object_or_404(Category, slug=slug)
+    return render_to_response('charleston/category_detail.html',
+                              {'object_list': category.entry_set.all(),
+                               'category': category})
